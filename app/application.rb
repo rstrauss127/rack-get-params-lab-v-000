2eec6a11edbf4b1135fee7c3a78a6b732c9d7967
Rebecca Strauss <rstrauss127@gmail.com>
2018-10-28 23:35:09 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -22,6 +22,15 @@ class Application
           resp.write "#{item}\n"
         end
       end
+
+    elsif req.path.match(/add/)
+      add_term = req.params["item"]
+      if @@items.include?(add_term)
+        @@cart << add_term
+        resp.write "added #{add_term}"
+      else
+        resp.write "We don't have that item"
+      end
     else
       resp.write "Path Not Found"
     end
